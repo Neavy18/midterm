3175dac8a148ba147ba947969308d9ea3738a879
michael <michael.james.ashworth@gmail.com>
2022-01-13 17:06:54 +0000

    setup rough skeleton to begin html/css styling

--- db/seeds/02_to_do_list.sql
+++ db/seeds/02_to_do_list.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO to_do_lists (name, category_id) VALUES ('Harry Potter', 1);
-INSERT INTO to_do_lists (name, category_id) VALUES ('Test 1', 2);
-INSERT INTO to_do_lists (name, category_id) VALUES ('Test 2', 3);
-INSERT INTO to_do_lists (name, category_id) VALUES ('Test 3', 4);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Masks', 2);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Lord of the Rings', 3);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Otto Bistro', 4);
 
