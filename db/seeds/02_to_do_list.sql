a9426457795ef03e2245e198da80347efa8ad624
Neavy18 <genevieve.gt18@gmail.com>
2022-01-13 20:48:55 -0500

    Add second CSS version with designed body and tables

--- db/seeds/02_to_do_list.sql
+++ db/seeds/02_to_do_list.sql
@@ -3,4 +3,14 @@ INSERT INTO to_do_lists (name, category_id) VALUES ('Harry Potter', 1);
 INSERT INTO to_do_lists (name, category_id) VALUES ('Masks', 2);
 INSERT INTO to_do_lists (name, category_id) VALUES ('Lord of the Rings', 3);
 INSERT INTO to_do_lists (name, category_id) VALUES ('Otto Bistro', 4);
+INSERT INTO to_do_lists (name, category_id) VALUES ('The Metamorphosis', 1);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Milk', 2);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Godfather', 3);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Aloette Restaurant', 4);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Giovannis Room', 1);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Mugs', 2);
+INSERT INTO to_do_lists (name, category_id) VALUES ('The wizard of Oz', 3);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Joe Beef', 4);
+
+
 
