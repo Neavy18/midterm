f5aee77c7ed18d6efa714a37e17fac3fa392d53d
Neavy18 <genevieve.gt18@gmail.com>
2022-01-13 22:25:06 -0500

    Add proper seeds for presentation

--- db/seeds/02_to_do_list.sql
+++ db/seeds/02_to_do_list.sql
@@ -1,16 +1,20 @@
 -- Users table seeds here (Example)
 INSERT INTO to_do_lists (name, category_id) VALUES ('Harry Potter', 1);
-INSERT INTO to_do_lists (name, category_id) VALUES ('Masks', 2);
+INSERT INTO to_do_lists (name, category_id) VALUES ('4k TV', 2);
 INSERT INTO to_do_lists (name, category_id) VALUES ('Lord of the Rings', 3);
 INSERT INTO to_do_lists (name, category_id) VALUES ('Otto Bistro', 4);
 INSERT INTO to_do_lists (name, category_id) VALUES ('The Metamorphosis', 1);
-INSERT INTO to_do_lists (name, category_id) VALUES ('Milk', 2);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Macbook', 2);
 INSERT INTO to_do_lists (name, category_id) VALUES ('Godfather', 3);
 INSERT INTO to_do_lists (name, category_id) VALUES ('Aloette Restaurant', 4);
 INSERT INTO to_do_lists (name, category_id) VALUES ('Giovannis Room', 1);
-INSERT INTO to_do_lists (name, category_id) VALUES ('Mugs', 2);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Headset', 2);
 INSERT INTO to_do_lists (name, category_id) VALUES ('The wizard of Oz', 3);
-INSERT INTO to_do_lists (name, category_id) VALUES ('Joe Beef', 4);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Ryu Sushi', 4);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Normal People', 1);
+INSERT INTO to_do_lists (name, category_id) VALUES ('PS5', 2);
+INSERT INTO to_do_lists (name, category_id) VALUES ('Spider Man No Way Home', 3);
+INSERT INTO to_do_lists (name, category_id) VALUES ('La Trattoria', 4);
 
 
 
